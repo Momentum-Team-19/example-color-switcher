482d0d4032a8f1982404a4bdd414f82cdbd2a5d7
Amy Gori <amydgori@gmail.com>
2023-09-06 15:03:24 -0400

    Add state setting functions for each color button

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('papayawhip')
   // when I click a button, the background color of the page changes to that color
   // need to click on a button and know what color to change to
   // mechanism for changing the background color
 
   const handleClickonOrangeButton = () => {
     console.log('You clicked orange')
+    /// I can set state here for backgroundColor
+    setBackgroundColor('orange')
   }
 
   const handleClickonTealButton = () => {
     console.log('You clicked teal')
+    setBackgroundColor('teal')
   }
 
   const handleClickonPurpleButton = () => {
     console.log('You clicked purple')
+    setBackgroundColor('purple')
   }
 
   const handleClickonRedButton = () => {
     console.log('You clicked red')
+    setBackgroundColor('red')
   }
 
   const handleClickonGreenButton = () => {
     console.log('You clicked green')
+    setBackgroundColor('green')
   }
 
   return (
-    <div id="controls" className="container">
+    <div id="controls" className={'container ' + backgroundColor}>
       <button onClick={handleClickonOrangeButton}>Orange</button>
       <button onClick={handleClickonPurpleButton}>Purple</button>
       <button onClick={handleClickonTealButton}>Teal</button>
